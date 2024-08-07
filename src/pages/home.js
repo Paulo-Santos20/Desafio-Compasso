5ea71b2e55805dca4fa2bd8b8bdf7bdec721bb34
Paulo-Santos20 <paulo_santos20@outlook.com.br>
2024-08-07 17:41:41 -0300

    style> font

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,17 @@
 // src/pages/Home.js
-import React from 'react';
-import '../styles/home.css';
-import Notebook from '../assets/home-photo.png';
+import React from "react";
+import "../styles/home.css";
+import Notebook from "../assets/home-photo.png";
 const Home = () => {
   return (
     <section className="container">
-      <div className="left-side">
-        <h2>Login</h2>
+      <div className="signin">
+        <div className="login">
+        <h1>Olá</h1>
+          <h2>Para continuar navegando de forma segura, efetue o login na rede.</h2>
+        
         <form>
+        <h3>Login</h3>
           <div className="form-group">
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" />
@@ -19,6 +23,8 @@ const Home = () => {
           <button type="submit">Login</button>
         </form>
       </div>
+        </div>
+        
       <div className="right-side">
         <img src={Notebook} alt="Placeholder" />
       </div>
